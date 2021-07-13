02bc4e5b420bf4f66f8ac9e460733132dd486d65
Juhyun-Jeong <wjdeoaks92@naver.com>
2021-07-13 22:26:16 +0900

    Init Commit

---
+++ src/test/java/com/springboot/toyproject/todolist/SpringbootToyprojectTodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.toyproject.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootToyprojectTodolistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
